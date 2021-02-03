2c00f9db74c4a896f7c5b92c2b61c45d4b911c7f
Alexandra Stricklin <stricklin927@gmail.com>
2021-02-03 00:58:22 -0500

    update PORT settings

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ const logger = require("morgan");
 const mongoose = require("mongoose");
 const compression = require("compression");
 
-const PORT = 3000;
+const PORT = process.env.PORT || 3000;
 
 const app = express();
 
@@ -16,14 +16,13 @@ app.use(express.json());
 app.use(express.static("public"));
 
 mongoose.connect(
-  process.env.MONGODB_URI || 'mongodb://localhost/Transactions',
+  process.env.MONGODB_URI || "mongodb://localhost/Transaction",
   {
     useNewUrlParser: true,
     useUnifiedTopology: true,
     useCreateIndex: true,
     useFindAndModify: false
-  }
-);
+  });
 
 // routes
 app.use(require("./routes/api.js"));
