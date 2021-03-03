6c2798fc9859cc72b88cd74d3b07a8a177281077
Alexandra Stricklin <stricklin927@gmail.com>
2021-03-03 00:33:56 -0500

    edit service worker code

--- public/service-worker.js
+++ public/service-worker.js
@@ -1,71 +1,74 @@
-const CACHE_NAME = "static-cache-v1";
+const CACHE_NAME = "static-cache-v2";
 const DATA_CACHE_NAME = "data-cache-v1";
 const FILES_TO_CACHE = [
-    "/",
-    "/index.html",
-    "/assets/css/styles.css",
-    "/assets/js/index.js",
-    "/assets/js/db.js",
-    "/assets/images/icons/icon-192x192.png",
-    "/assets/images/icons/icon-512x512.png",
-    "https://stackpath.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css",
-    "https://cdn.jsdelivr.net/npm/chart.js@2.8.0"
-  ];
-    
-  self.addEventListener("install", function(evt) {
+  "/",
+  "/index.html",
+  "/index.js",
+  "/styles.css",
+  "/icons/icon-192x192.png",
+  "/icons/icon-512x512.png"
+];
+
+// Install and register service worker
+self.addEventListener("install", function(evt) {
+    // evt.waitUntil(
+    //     caches.open(DATA_CACHE_NAME).then((cache) => cache.add("/api/transaction"))
+    // );
+
     evt.waitUntil(
-      caches.open(CACHE_NAME).then((cache) => {
-        return cache.addAll(FILES_TO_CACHE);
-      })
+        caches.open(CACHE_NAME).then((cache) => cache.addAll(FILES_TO_CACHE))
     );
-  
+
     self.skipWaiting();
-  });
-  
-  self.addEventListener("activate", function(evt) {
+});
+
+// Activate service worker & remove old cached data
+self.addEventListener("activate", function(evt) {
     evt.waitUntil(
-      caches.keys().then((keyList) => {
-        return Promise.all(
-          keyList.map((key) => {
-            if (key !== CACHE_NAME && key !== DATA_CACHE_NAME) {
-              return caches.delete(key);
-            }
-          })
-        );
-      })
+        caches.keys().then(keyList => {
+            return Promise.all(
+                keyList.map(key => {
+                    if (key !== CACHE_NAME && key !== DATA_CACHE_NAME) {
+                        console.log("Removing old cache data", key);
+                        return caches.delete(key);
+                    }
+                })
+            );
+        })
     );
-  
+
     self.clients.claim();
-  });
-  
-  self.addEventListener("fetch", function(evt) {
-    if (evt.request.url.includes("/api/transaction")) {
-      evt.respondWith(
-        caches
-          .open(DATA_CACHE_NAME)
-          .then((cache) => {
-            return fetch(evt.request)
-              .then((response) => {
-                if (response.status === 200) {
-                  cache.put(evt.request.url, response.clone());
-                }
-  
-                return response;
-              })
-              .catch(err => {
-                return cache.match(evt.request);
-              });
-          })
-          .catch((err) => console.log(err))
-      );
-  
-      return;
+});
+
+// Enable service worker to intercept network requests
+self.addEventListener("fetch", function(evt) {
+    if (evt.request.url.includes("/api/")) {
+        console.log("[Service Worker] Fetch (data)", evt.request.url);
+
+        evt.respondWith(
+            caches.open(DATA_CACHE_NAME).then(cache => {
+                return fetch(evt.request)
+                    .then(response => {
+                        if (response.status ===200) {
+                            cache.put(evt.request.url, response.clone());
+                        }
+
+                        return response;
+                    })
+                    .catch(err => {
+                        return cache.match(evt.request);
+                    });
+            }).catch(err => console.log(err))
+        );
+
+        return;
     }
-  
-    // going offline
+
     evt.respondWith(
-      caches.match(evt.request).then((response) => {
-        return response || fetch(evt.request);
-      })
+        caches.open(CACHE_NAME).then(cache => {
+            return cache.match(evt.request).then(response => {
+              return response || fetch(evt.request);
+            });
+          })
     );
-  });
+});
